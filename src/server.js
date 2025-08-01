040412c4e09754dfcd941799cc1fca29b23abfad
Valya Vovchenko <valya253@ukr.net>
2025-08-01 23:37:33 +0300

    fixed error in patch request

--- src/server.js
+++ src/server.js
@@ -13,6 +13,7 @@ export function setupServer() {
   const app = express();
 
   app.use(express.json());
+
   app.use(cors());
   app.use(cookieParser());
   app.use('/uploads', express.static(UPLOAD_DIR));

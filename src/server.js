923fc509bbfec5df02bda0c9a3ab64261a767296
Valya Vovchenko <valya253@ukr.net>
2025-08-05 16:12:26 +0300

    added documentation for our API using Swagger

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import { errorHandler } from './middlewares/errorHandler.js'
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 export function setupServer() {
   const app = express();
@@ -17,6 +18,8 @@ export function setupServer() {
   app.use(cors());
   app.use(cookieParser());
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
+  
   const logger = pino({
     transport: {
       target: 'pino-pretty',
